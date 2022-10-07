469eb24a59cbd8cfb60b4cfbf4cf7d1d47ac9cfd
Charitha <Charitha.Neelisetty@ivycomptech.com>
2022-10-07 19:47:21 +0530

    Q4 uploaded

---
+++ Evaluation_Q4/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Evaluation_Q4
+{
+    public abstract class Bird
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public abstract void eat();
+        public void fly()
+        {
+            Console.WriteLine("Bird  is flying");
+        }
+
+    }
+    public class Parrot : Bird
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Parrot is eating");
+        }
+        public new void fly()
+        {
+            Console.WriteLine("Parrot is Flying");
+        }
+    }
+    public class Crow : Bird
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Crow is eating");
+        }
+        public new void fly()
+        {
+            Console.WriteLine("Crow is Flying");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter type of bird");
+            string bird = Console.ReadLine();
+            switch (bird)
+            {
+                case "parrot":
+                    Parrot p = new Parrot();
+                    Console.WriteLine("Enter name of bird");
+                    string bird_name1 = Console.ReadLine();
+                    p.Name = bird_name1;
+                    Console.WriteLine(p.Name);
+                    p.fly();
+                    p.eat();
+                    break;
+
+                case "crow":
+                    Crow c = new Crow();
+                    Console.WriteLine("Enter name of bird");
+                    string bird_name2 = Console.ReadLine();
+                    c.Name = bird_name2;
+                    Console.WriteLine(c.Name);
+                    c.fly();
+                    c.eat();
+                    break;
+
+            }
+        }
+    }
+}
