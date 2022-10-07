8b72d643d63ca550df3f0398d8fef5cad32ba047
Charitha <Charitha.Neelisetty@ivycomptech.com>
2022-10-07 20:07:35 +0530

    Q6 uploaded

---
+++ Evaluation_Q6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Evaluation_Q6
+{
+    class Copy_File
+
+    {
+
+
+
+        static void Main()
+
+        {
+
+            // Copy contents from file1 to file2
+
+            File.Copy("sample1.txt", "sample2.txt");
+
+
+
+
+
+            // Display file2 contents
+
+            Console.WriteLine(File.ReadAllText("sample2.txt"));
+
+            var file = File.ReadAllText(@"C:\Users\Charitha.Neelisetty\source\repos\C Sharp Evaluation\Evaluation_Q6\Evaluation_Q6\bin\Debug\netcoreapp3.1\sample2.txt");
+
+            FileInfo filetodecrypt = new FileInfo(file);
+
+            filetodecrypt.Encrypt(); //Encrypt
+
+
+
+            filetodecrypt.Decrypt(); //Decrypt
+
+
+
+        }
+
+    }
+}
